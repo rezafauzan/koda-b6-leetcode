f378584f5a77e2f34cdc6ad0aa4771758fddd4d2
rezafauzan593@gmail.com <rezafauzan593@gmail.com>
2026-01-07 22:56:30 +0700

    add 75. Sort Colors

---
+++ taskmedium2.js
@@ -0,0 +1,35 @@
+// 75. Sort Colors
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function (nums) {
+    let countRed = 0
+    let countWhite = 0
+    let countBlue = 0
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === 0) {
+            countRed++
+        } else if (nums[i] === 1) {
+            countWhite++
+        }
+        else {
+            countBlue++
+        }
+    }
+
+    let index = 0
+
+    for (let i = 0; i < countRed; i++) {
+        nums[index++] = 0
+    }
+
+    for (let i = 0; i < countWhite; i++) {
+        nums[index++] = 1
+    }
+
+    for (let i = 0; i < countBlue; i++) {
+        nums[index++] = 2
+    }
+}
